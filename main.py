df2ae226eb792dc2fe25380accad8dd57e22420b
aa-ag <aaron.aguerrevere@gmail.com>
2021-01-13 05:13:55 -0600

    encrypt() working

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 ###--- IMPORTS ---###
 from cryptography.fernet import Fernet
+import base64
 import config
 
 
 ###--- FUNCTIONS ---###
 
+def write_key():
+    '''
+     Generates a key, and saves it to a config.py file
+    '''
+    key = Fernet.generate_key()
+    with open("config.py", "wb") as kf:
+        kf.write(key)
+
+
 def encrypt():
-    pass
+    '''
+     Encrypts message using Fernet class
+    '''
+    # generate key
+    key = Fernet.generate_key()
+
+    # initialize Fernet class
+    fernet = Fernet(key)
+
+    # convert message to bytes by encoding it
+    message = "Hello, World!".encode()
+
+    # encrypt message
+    token = fernet.encrypt(message)
+
+    print(token)
 
 
 def decrypt():
@@ -15,4 +40,6 @@ def decrypt():
 
 ###--- DRIVER CODE ---###
 if __name__ == "__main__":
-    pass
+    # write_key()
+    encrypt()
+    # pass
