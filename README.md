ea371ae5a816434ba0f8dbbfc443604db5a43b67
aa-ag <aaron.aguerrevere@gmail.com>
2021-01-16 19:06:50 -0600

    sending emails

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ Quick en/decrypting Python script.
 # To run
 
 `conda activate endecrypter`
+
+# Sending encrypted emails
+
+To use `send_encrypted_email.py`, 
+
+1. "less secure app access" needs to be allowed on [Google Account](https://myaccount.google.com/lesssecureapps).
+
+2. Two-factor authentication must be [Turned Off](https://support.google.com/accounts/answer/1064203?co=GENIE.Platform%3DDesktop&hl=en).
+
+Interesting side-note. This script was originally named `email.py`, and that created this problem: `https://stackoverflow.com/questions/33313858/importerror-no-module-named-email-mime-email-is-not-a-package`.
---
+++ send_encrypted_email.py
@@ -0,0 +1,37 @@
+###--- IMPORTS ---###
+import smtplib  # https://docs.python.org/3/library/smtplib.html
+import settings
+
+###--- FUNCTIONS ---###
+
+
+def send_encrypted_email():
+    # Simple Mail Transfer Protocol ("SMTP") session for gmail
+    # An SMTP instance encapsulates an SMTP connection.
+    session = smtplib.SMTP('smtp.gmail.com', 587)
+
+    # Put the SMTP connection in TLS (Transport Layer Security) mode.
+    # All SMTP commands that follow will be encrypted.
+    session.starttls()
+
+    # auth gmail
+    session.login(f"{settings.sending_from}", f"{settings.hahaha}")
+
+    # body of email/message that will be sent
+    message_body = ""
+
+    encrypt_message_body = input("\nEnter the message to encrypt:\n>>")
+
+    for i in range(len(encrypt_message_body)):
+        message_body = message_body + chr(ord(encrypt_message_body[i]))
+
+    # send encrypted message in an email
+    session.sendmail(f"{settings.sending_from}",
+                     f"{settings.to}", message_body)
+
+    session.quit()
+
+
+###--- DRIVER CODE ---###
+if __name__ == "__main__":
+    send_encrypted_email()
